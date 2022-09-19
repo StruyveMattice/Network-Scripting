b71c6fd62b9259d208677c670dd6220641cf293d
Mattice Struyve <55881660+StruyveMattice@users.noreply.github.com>
2022-09-19 15:48:01 +0200

    Added Static IP Script

---
+++ Powershell/Setting Static IP.ps1
@@ -0,0 +1,7 @@
+﻿$ip = Read-Host -Prompt 'IP Address'
+$subnet = Read-Host -Prompt 'Network Prefix'
+$default_gateway = Read-Host -Prompt 'Default Gateway'
+$pref_dns = Read-Host -Prompt 'Preffered DNS'
+$altr_dns = Read-Host -Prompt 'Alternate DNS'
+
+New-NetIPAddress –IPAddress $ip -DefaultGateway $default_gateway -PrefixLength $subnet Set-DnsClientServerAddress -ServerAddresses $pref_dns, $altr_dns -InterfaceIndex (Get-NetAdapter).6
