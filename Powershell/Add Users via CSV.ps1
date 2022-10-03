09b6b883cc8ef2ceb185d98f784df2023514a071
Mattice Struyve <55881660+StruyveMattice@users.noreply.github.com>
2022-10-03 12:32:07 +0200

    Added option to del and add users via CSV

---
+++ Powershell/Add Users via CSV.ps1
@@ -0,0 +1,9 @@
+﻿$users = Import-Csv -Path 'C:\Users\Administrator\Documents\CSV Files\UserAccounts.csv' -Delimiter(';')
+$Secure_String_Pwd = ConvertTo-SecureString "P@ssw0rd" -AsPlainText -Force
+
+foreach($item in $users)
+{
+    $name = $item.Name
+    New-ADUser -Name $item.Name -SamAccountName $item.SamAccountName -GivenName $item.GivenName -Surname $item.Surname -DisplayName $item.DisplayName -AccountPassword $Secure_String_Pwd -HomeDrive $item.HomeDrive -HomeDirectory $item.HomeDirectory -ScriptPath $item.ScriptPath -Path $item.Path -PasswordNeverExpires $true -Enabled $true
+    Write-Host "$name was added"
+}
---
+++ Powershell/Remove AD User.ps1
@@ -0,0 +1,9 @@
+﻿$users = Import-Csv -Path 'C:\Users\Administrator\Documents\CSV Files\UserAccounts.csv' -Delimiter(';')
+
+foreach($item in $users)
+{
+    $name = $item.Name
+    Remove-ADUser -Identity $item.SamAccountName -Confirm:$false
+    Write-Host "$name was deleted"
+    
+}
