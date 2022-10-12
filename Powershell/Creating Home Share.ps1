758df0ff132a64bbc1e1b26727bad5525ed53834
Mattice Struyve <55881660+StruyveMattice@users.noreply.github.com>
2022-10-12 09:39:13 +0200

    Update Creating Home Share.ps1

--- Powershell/Creating Home Share.ps1
+++ Powershell/Creating Home Share.ps1
@@ -1,5 +1,5 @@
 Write-Host "Creating Home Share on MS..."
 
-New-SmbShare -Name "Home" -Path "\\MS\" -FullAccess "Everyone", "INTRANET\Administrators" -ReadAccess "Authenticated Users"
+New-SmbShare -Name "Homes" -Path "C:\" -FullAccess "Everyone"
 
 Write-Host "Home Share Created"
