ff2f80bfbbf9cd57377decb605e1ec4e8733c077
Mattice Struyve <55881660+StruyveMattice@users.noreply.github.com>
2022-10-12 16:21:26 +0200

    Finished Opdracht 1

---
+++ Powershell/Opdracht 1/Configuring DHCP Failover.ps1
@@ -0,0 +1,3 @@
+﻿$Secure_String_Pwd = ConvertTo-SecureString "P@ssw0rd" -AsPlainText -Force
+
+Add-DhcpServerv4Failover -Name "dc01.intranet.mijnschool.be" -PartnerServer "dc02.intranet.mijnschool.be" -ScopeId 192.168.1.0 -SharedSecret $Secure_String_Pwd -LoadBalancePercent 50
